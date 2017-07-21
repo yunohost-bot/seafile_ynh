9380ee33c1041ec3228c9e688def5d0d070136dc
Josué Tille <josue@tille.ch>
2017-07-21 14:56:41 +0200

    Full app Update

--- conf/update_sso_conf.py
+++ conf/update_sso_conf.py
@@ -2,6 +2,12 @@ import json
 
 with open("/etc/ssowat/conf.json.persistent", "r") as jsonFile:
     data = json.load(jsonFile)
+    data["unprotected_urls"].remove("/seafhttp")
+    data["unprotected_urls"].remove("/seafdav")
+    if "skipped_urls" in data:
+        data["skipped_urls"].append("/seafhttp")
+    else:
+        data["skipped_urls"] = ["/seafhttp"]
     data["skipped_urls"].append("/seafdav")
 
 with open("/etc/ssowat/conf.json.persistent", "w") as jsonFile:
--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,84 +1,98 @@
 #!/bin/bash 
 
-seafile_version=6.0.9
+# Retrieve arguments
+app=$YNH_APP_INSTANCE_NAME
+
+seafile_version=6.1.1
 
 ## Adapt md5sum while you update app
-x86_64sum="8d1547c51feecaff757eb64381ce154c"
-i386sum="8a0b9e08d9c80790752b435c3e148328"
-armsum="15ca4b69585a990e00474e4cfc4be9eb"
+x86_64sum="3f887d018bd7eaa8b4e1e7830f365802311686636227f1c08a8c48e89aefc35c"
+i386sum="6d236c93f5a5f674c52b943428995cfe046965f17f2df9f644e17a704072603b"
+armsum="e40a8f3a91a4629428288aaabe4a2d6906dd00afc08faf08916f30b8c5a312e4"
 
-init_script() {
-    # Exit on command errors and treat unset variables as an error
-    set -eu
+# Detect the system architecture to download the right tarball
+# NOTE: `uname -m` is more accurate and universal than `arch`
+# See https://en.wikipedia.org/wiki/Uname
+if [ -n "$(uname -m | grep 64)" ]; then
+	architecture="x86-64"
+elif [ -n "$(uname -m | grep 86)" ]; then
+	architecture="i386"
+elif [ -n "$(uname -m | grep arm)" ]; then
+	architecture="arm"
+else
+	ynh_die "Unable to detect your achitecture, please open a bug describing \
+        your hardware and the result of the command \"uname -m\"." 1
+fi
 
-    # Source YunoHost helpers
-    source /usr/share/yunohost/helpers
-
-    # Retrieve arguments
-    app=$YNH_APP_INSTANCE_NAME
+get_configuration() {
+	final_path=$(ynh_app_setting_get $app final_path)
+	seafile_user=$(ynh_app_setting_get $app seafile_user)
+	
+	if [[ -z $final_path ]] || [[ -z $seafile_user ]]
+	then
+		if [[ -e /var/www/$app ]]
+		then
+			final_path=/var/www/$app
+			seafile_user=www-data
+		elif [[ -e /opt/yunohost/$app ]]
+		then
+			final_path=/opt/yunohost/$app
+			seafile_user=seafile
+		else
+			ynh_die "Error : can't find seafile path"
+		fi
+		ynh_app_setting_set $app final_path $final_path
+		ynh_app_setting_set $app seafile_user $seafile_user
+    fi
 }
 
-set_configuration() {
-    if [[ -e /var/www/$app ]]
-    then
-        final_path=/var/www/$app
-        seafile_user=www-data
-    elif [[ -e /opt/yunohost/$app ]]
-    then
-        final_path=/opt/yunohost/$app
-        seafile_user=seafile
-    else
-        ynh_die "Error : can't find seafile path"
-    fi
+config_nginx() {
+	ynh_replace_string PATHTOCHANGE1 $path ../conf/nginx.conf
+	ynh_replace_string PATHTOCHANGE2 $path2 ../conf/nginx.conf
+	ynh_replace_string ALIASTOCHANGE $final_path/ ../conf/nginx.conf
+	ynh_replace_string SEAHUB_PORT $seahub_port ../conf/nginx.conf
+	ynh_replace_string SEAFILE_FILESERVER_PORT $fileserver_port ../conf/nginx.conf
+	ynh_replace_string WEBDAV_PORT $webdav_port ../conf/nginx.conf
+	cp ../conf/nginx.conf /etc/nginx/conf.d/$domain.d/seafile.conf
+	
+	systemctl reload nginx.service
 }
 
 get_source() {
-    if [[ $1 == 'rpi' ]]
+    if [[ $1 == 'arm' ]]
     then
         wget -q -O '/tmp/seafile_src.tar.gz' 'https://github.com/haiwen/seafile-rpi/releases/download/v'$2'/seafile-server_'$2'_stable_pi.tar.gz'
-        md5sum=$armsum
+        sha256sum=$armsum
     elif [[ $1 == 'x86-64' ]]
     then
         wget -q -O '/tmp/seafile_src.tar.gz' 'https://download.seadrive.org/seafile-server_'$2'_x86-64.tar.gz'
-        md5sum=$x86_64sum
+        sha256sum=$x86_64sum
     else
         wget -q -O '/tmp/seafile_src.tar.gz' 'https://download.seadrive.org/seafile-server_'$2'_i386.tar.gz'
-        md5sum=$i386sum
+        sha256sum=$i386sum
     fi
 
-    if [[ ! -e '/tmp/seafile_src.tar.gz' ]] || [[ $(md5sum '/tmp/seafile_src.tar.gz' | cut -d' ' -f1) != $md5sum ]]
+    if [[ ! -e '/tmp/seafile_src.tar.gz' ]] || [[ $(sha256sum '/tmp/seafile_src.tar.gz' | cut -d' ' -f1) != $sha256sum ]]
     then
         ynh_die "Error : can't get seafile source"
     fi
 }
 
-CHECK_VAR () {	# Vérifie que la variable n'est pas vide.
-# $1 = Variable à vérifier
-# $2 = Texte à afficher en cas d'erreur
-	test -n "$1" || (echo "$2" >&2 && false)
+extract_source() {
+	mkdir -p $final_path/seafile-server-$seafile_version
+	tar xzf '/tmp/seafile_src.tar.gz'
+	mv seafile-server-$seafile_version/* $final_path/seafile-server-$seafile_version
+	mv '/tmp/seafile_src.tar.gz' $final_path/installed/seafile-server_${seafile_version}.tar.gz
 }
 
-CHECK_PATH () {	# Vérifie la présence du / en début de path. Et son absence à la fin.
-	if [ "${path:0:1}" != "/" ]; then    # Si le premier caractère n'est pas un /
-		path="/$path"    # Ajoute un / en début de path
-	fi
-	if [ "${path:${#path}-1}" == "/" ] && [ ${#path} -gt 1 ]; then    # Si le dernier caractère est un / et que ce n'est pas le seul caractère.
-		path="${path:0:${#path}-1}"	# Supprime le dernier caractère
-	fi
+install_dependance() {
+	ynh_install_app_dependencies python2.7 python-setuptools python-simplejson python-imaging python-mysqldb python-flup expect ffmpeg python-requests
+	pip install pillow moviepy
 }
 
-# Find a free port and return it
-#
-# example: port=$(ynh_find_port 8080)
-#
-# usage: ynh_find_port begin_port
-# | arg: begin_port - port to start to search
-ynh_find_port () {
-	port=$1
-	test -n "$port" || ynh_die "The argument of ynh_find_port must be a valid port."
-	while netcat -z 127.0.0.1 $port       # Check if the port is free
-	do
-		port=$((port+1))	# Else, pass to next port
-	done
-	echo $port
+ynh_clean_setup () {
+	killall seafile-controller
+	killall seaf-server
+	killall ccnet-server
+	kill $(ps -C python2.7 -o pid,cmd | grep "python2.7 $final_path/seafile-server-$seafile_version/seahub/manage.py" | cut -d' ' -f1)
 }
