494ca2e218f0f0255faad2f99bf62df4e2d50d96
Josué Tille <josue@tille.ch>
2018-02-11 01:20:58 +0100

    Add title for each part of code and use helper upstream_version

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,65 +1,13 @@
-#!/bin/bash 
+#=================================================
+# SET ALL CONSTANTS
+#=================================================
 
-# Retrieve arguments
 app=$YNH_APP_INSTANCE_NAME
-
-# Detect the system architecture to download the right tarball
-# NOTE: `uname -m` is more accurate and universal than `arch`
-# See https://en.wikipedia.org/wiki/Uname
-if [ -n "$(uname -m | grep 64)" ]; then
-	architecture="x86-64"
-elif [ -n "$(uname -m | grep 86)" ]; then
-	architecture="i386"
-elif [ -n "$(uname -m | grep arm)" ]; then
-	architecture="arm"
-else
-	ynh_die "Unable to detect your achitecture, please open a bug describing \
-        your hardware and the result of the command \"uname -m\"." 1
-fi
-
-# Read the value of a key in a ynh manifest file
-#
-# usage: ynh_read_manifest manifest key
-# | arg: manifest - Path of the manifest to read
-# | arg: key - Name of the key to find
-ynh_read_manifest () {
-	manifest="$1"
-	key="$2"
-	python3 -c "import sys, json;print(json.load(open('$manifest'))['$key'])"
-}
-
-# Read the upstream version from the manifest 
-# The version number in the manifest is defined by <upstreamversion>~ynh<packageversion>
-# For example : 4.3-2~ynh3
-# This include the number before ~ynh
-# In the last example it return 4.3-2
-#
-# usage: ynh_app_upstream_version
-ynh_app_upstream_version () {
-    manifest_path="../manifest.json"
-    if [ ! -e "$manifest_path" ]; then
-        manifest_path="../settings/manifest.json"	# Into the restore script, the manifest is not at the same place
-    fi
-    version_key=$(ynh_read_manifest "$manifest_path" "version")
-    echo "${version_key/~ynh*/}"
-}
-
-# Read package version from the manifest
-# The version number in the manifest is defined by <upstreamversion>~ynh<packageversion>
-# For example : 4.3-2~ynh3
-# This include the number after ~ynh
-# In the last example it return 3
-#
-# usage: ynh_app_package_version
-ynh_app_package_version () {
-    manifest_path="../manifest.json"
-    if [ ! -e "$manifest_path" ]; then
-        manifest_path="../settings/manifest.json"	# Into the restore script, the manifest is not at the same place
-    fi
-    version_key=$(ynh_read_manifest "$manifest_path" "version")
-    echo "${version_key/*~ynh/}"
-}
 seafile_version=$(ynh_app_upstream_version)
+
+#=================================================
+# DEFINE ALL COMMON FONCTIONS
+#=================================================
 
 get_configuration() {
 	final_path=$(ynh_app_setting_get $app final_path)
