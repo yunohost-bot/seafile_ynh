1e2e1742d745b4015f590b3b9acc21b1035ac56b
Josué Tille <josue@tille.ch>
2020-03-27 13:25:40 +0100

    Fix restoration error with pip cache

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -23,6 +23,8 @@ install_dependance() {
 	ynh_install_app_dependencies python2.7 python-pip libpython2.7 python-setuptools python-ldap python-urllib3 python-simplejson python-imaging python-mysqldb python-flup expect python-requests python-dev ffmpeg python-memcache \
         libjpeg62-turbo-dev zlib1g-dev # For building pillow
     ynh_add_swap 2000
+    # We need to do that because we can have some issue about the permission access to the pip cache without this
+    set_permission
     # Note that we install imageio to force the dependance, without this imageio 2.8 is installed and it need python3.5
     sudo -u $seafile_user pip install --user --upgrade Pillow 'moviepy<1.0' 'imageio<2.8' certifi idna
     ynh_del_swap
