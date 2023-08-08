548d59accf62f421a37db83dd1c31a6ffac15e15
Josué Tille <josue@tille.ch>
2023-08-08 23:21:11 +0200

    First draft packaging v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -2,50 +2,20 @@
 # SET ALL CONSTANTS
 #=================================================
 
-app=$YNH_APP_INSTANCE_NAME
+db_user=seafile
+time_zone=$(cat /etc/timezone)
 
 #=================================================
 # DEFINE ALL COMMON FONCTIONS
 #=================================================
 
-install_source() {
-    mkdir "$final_path/seafile-server-$seafile_version"
-    if [ $YNH_ARCH == "i386" ] || [ $YNH_ARCH == "armel" ]
-    then
-        ynh_die --message "Error : this architecture is no longer supported by the upstream. Please create en issue here : https://github.com/YunoHost-Apps/seafile_ynh/issues to ask to discuss about a support of this architecture"
-    fi
-    ynh_setup_source "$final_path/seafile-server-$seafile_version" "$YNH_ARCH"
-
-    # Drop some conflicting package cf: https://forum.seafile.com/t/seahub-fails-to-start-nameerror-name-mysql-is-not-defined/14566/3
-    ynh_secure_remove --file=$final_path/seafile-server-$seafile_version/seahub/thirdpart/MySQLdb
-    ynh_secure_remove --file=$final_path/seafile-server-$seafile_version/seahub/thirdpart/cffi
-    ynh_secure_remove --file=$final_path/seafile-server-$seafile_version/seahub/thirdpart/PIL
-}
-
-install_source_7_0() {
-    if ! [ -e $final_path/seafile-server-7.0.5 ]; then
-        mkdir "$final_path/seafile-server-7.0.5"
-        if [ $YNH_ARCH == "i386" ] || [ $YNH_ARCH == "armel" ]
-        then
-            ynh_die --message "Error : this architecture is no longer supported by the upstream. Please create en issue here : https://github.com/YunoHost-Apps/seafile_ynh/issues to ask to discuss about a support of this architecture"
-        fi
-        ynh_setup_source "$final_path/seafile-server-7.0.5" "$YNH_ARCH"_7_0
-    fi
-}
-
 install_dependance() {
-    ynh_install_app_dependencies python3 python3-setuptools python3-pip python3-requests python3-dev libmariadb-dev-compat libmariadb-dev \
-        expect ffmpeg \
-        memcached libmemcached-dev \
-        python3-scipy python3-matplotlib \
-        libjpeg62-turbo-dev zlib1g-dev libffi-dev  # For building pillow
     ynh_add_swap --size=2000
     # We need to do that because we can have some issue about the permission access to the pip cache without this
-    chown -R $seafile_user:$seafile_user $final_path
+    chown -R $seafile_user:$seafile_user $install_dir
 
     # Note that we install imageio to force the dependance, without this imageio 2.8 is installed and it need python3.5
     sudo -u $seafile_user pip3 install --user --no-warn-script-location --upgrade future mysqlclient PyMySQL Pillow pylibmc captcha Jinja2 SQLAlchemy psd-tools django-pylibmc django-simple-captcha python3-ldap pycryptodome==3.12.0 cffi==1.14.0
-    # TODO add dependance when upgrade to seafile 8: django==2.2.*
     ynh_del_swap
 }
 
@@ -57,14 +27,14 @@ mv_expect_scripts() {
 }
 
 set_permission() {
-    chown -R $seafile_user:$seafile_user $final_path
-    chmod -R g-wx,o= $final_path
-    setfacl -m user:www-data:rX $final_path
-    setfacl -m user:www-data:rX $final_path/seafile-server-$seafile_version
+    chown -R $seafile_user:$seafile_user $install_dir
+    chmod -R g-wx,o= $install_dir
+    setfacl -m user:www-data:rX $install_dir
+    setfacl -m user:www-data:rX $install_dir/seafile-server-$seafile_version
     # At install time theses directory are not available
-    test -e $final_path/seafile-server-latest/seahub && setfacl -m user:www-data:rX $final_path/seafile-server-latest/seahub
-    test -e $final_path/seafile-server-latest/seahub/media && setfacl -R -m user:www-data:rX $final_path/seafile-server-latest/seahub/media
-    test -e $final_path/seahub-data && setfacl -R -m user:www-data:rX $final_path/seahub-data
+#REMOVEME?     test -e $install_dir/seafile-server-latest/seahub && setfacl -m user:www-data:rX $install_dir/seafile-server-latest/seahub
+#REMOVEME?     test -e $install_dir/seafile-server-latest/seahub/media && setfacl -R -m user:www-data:rX $install_dir/seafile-server-latest/seahub/media
+#REMOVEME?     test -e $install_dir/seahub-data && setfacl -R -m user:www-data:rX $install_dir/seahub-data
 
     # check that this directory exist because in some really old install the data could still be in the main seafile directory
     # We also check at the install time when data directory is not already initialised
@@ -72,11 +42,4 @@ set_permission() {
         chown -R $seafile_user:$seafile_user /home/yunohost.app/seafile-data
         chmod -R o= /home/yunohost.app/seafile-data
     fi
-}
-
-ynh_clean_setup () {
-	pkill -f seafile-controller
-	pkill -f seaf-server
-	pkill -f ccnet-server
-	pkill -f seahub
 }
