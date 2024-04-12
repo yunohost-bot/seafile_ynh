7d8568c2d1364cd1df3b3997c573bdd2c8cf935e
Josué Tille <josue@tille.ch>
2024-04-12 21:21:23 +0200

    Update experimental helper

--- scripts/experimental_helper.sh
+++ scripts/experimental_helper.sh
@@ -1,99 +1,135 @@
-#!/bin/bash
-
-# Add swap
+# Create a dedicated config file from a jinja template
 #
-# usage: ynh_add_swap --size=SWAP in Mb
-# | arg: -s, --size= - Amount of SWAP to add in Mb.
-ynh_add_swap () {
-	# Declare an array to define the options of this helper.
-	declare -Ar args_array=( [s]=size= )
-	local size
-	# Manage arguments with getopts
-	ynh_handle_getopts_args "$@"
-
-	local swap_max_size=$(( $size * 1024 ))
-
-	local free_space=$(df --output=avail / | sed 1d)
-	# Because we don't want to fill the disk with a swap file, divide by 2 the available space.
-	local usable_space=$(( $free_space / 2 ))
-
- 	SD_CARD_CAN_SWAP=${SD_CARD_CAN_SWAP:-0}
-
-	# Swap on SD card only if it's is specified
-	if ynh_is_main_device_a_sd_card && [ "$SD_CARD_CAN_SWAP" == "0" ]
-	then
-		ynh_print_warn --message="The main mountpoint of your system '/' is on an SD card, swap will not be added to prevent some damage of this one, but that can cause troubles for the app $app. If you still want activate the swap, you can relaunch the command preceded by 'SD_CARD_CAN_SWAP=1'"
-		return
-	fi
-
-	# Compare the available space with the size of the swap.
-	# And set a acceptable size from the request
-	if [ $usable_space -ge $swap_max_size ]
-	then
-		local swap_size=$swap_max_size
-	elif [ $usable_space -ge $(( $swap_max_size / 2 )) ]
-	then
-		local swap_size=$(( $swap_max_size / 2 ))
-	elif [ $usable_space -ge $(( $swap_max_size / 3 )) ]
-	then
-		local swap_size=$(( $swap_max_size / 3 ))
-	elif [ $usable_space -ge $(( $swap_max_size / 4 )) ]
-	then
-		local swap_size=$(( $swap_max_size / 4 ))
-	else
-		echo "Not enough space left for a swap file" >&2
-		local swap_size=0
-	fi
-
-	# If there's enough space for a swap, and no existing swap here
-	if [ $swap_size -ne 0 ] && [ ! -e /swap_$app ]
-	then
-		# Create file
-		truncate -s 0 /swap_$app
-
-		# set the No_COW attribute on the swapfile with chattr
-		chattr +C /swap_$app || true
-
-		# Preallocate space for the swap file, fallocate may sometime not be used, use dd instead in this case
-		if ! fallocate -l ${swap_size}K /swap_$app
-		then
-			dd if=/dev/zero of=/swap_$app bs=1024 count=${swap_size}
-		fi
-		chmod 0600 /swap_$app
-		# Create the swap
-		mkswap /swap_$app
-		# And activate it
-		swapon /swap_$app || true
-		# Then add an entry in fstab to load this swap at each boot.
-		echo -e "/swap_$app swap swap defaults 0 0 #Swap added by $app" >> /etc/fstab
-	fi
-}
-
-ynh_del_swap () {
-	# If there a swap at this place
-	if [ -e /swap_$app ]
-	then
-		# Clean the fstab
-		sed -i "/#Swap added by $app/d" /etc/fstab
-		# Desactive the swap file
-		swapoff /swap_$app || true
-		# And remove it
-		rm /swap_$app
-	fi
-}
-
-# Check if the device of the main mountpoint "/" is an SD card
+# usage: ynh_add_jinja_config --template="template" --destination="destination"
+# | arg: -t, --template=     - Template config file to use
+# | arg: -d, --destination=  - Destination of the config file
+# | arg: -i, --ignore_vars=  - List separated by space of script variables to ignore and don't pass in the jinja context.
+# |                            This could be useful mainly for special share which can't be retried by reference name (like the array).
 #
-# [internal]
+# examples:
+#    ynh_add_jinja_config --template="app.conf" --destination="$install_dir/app.conf"
+#    ynh_add_jinja_config --template="app-env" --destination="$install_dir/app-env" --ignore_vars="complex_array yolo"
 #
-# return 0 if it's an SD card, else 1
-ynh_is_main_device_a_sd_card () {
-	local main_device=$(lsblk --output PKNAME --noheadings $(findmnt / --nofsroot --uniq --output source --noheadings --first-only))
+# The template can be by default the name of a file in the conf directory
+#
+# The helper will verify the checksum and backup the destination file
+# if it's different before applying the new template.
+#
+# And it will calculate and store the destination file checksum
+# into the app settings when configuration is done.
+#
+##
+## About the variables passed to the template:
+##
+#
+# All variable defined in the script are available into the template (as string) except someone described below.
+# If a variable make crash the helper for some reason (by example if the variable is of type array)
+# or you just want to don't pass a specific variable for some other reason you can add it in the '--ignore_vars=' parameter as described above.
+# Here are the list of ignored variable and so there won't never be available in the template:
+# - All system environment variable like (TERM, USER, PATH, LANG, etc).
+#   If you need someone you just need to declare an other variable with the same value.
+#   Note that all Yunohost variable whose name begins by 'YNH_' are available and can be used in the template.
+# - This following list:
+#        legacy_args args_array template destination ignore_vars template_path python_env_var ignore_var_regex
+#        progress_scale progress_string0 progress_string1 progress_string2
+#        old changed binds types file_hash formats
+#
+##
+## Usage in templates:
+##
+#
+# For a full documentation of the template you can refer to: https://jinja.palletsprojects.com/en/3.1.x/templates/
+# In Yunohost context there are no really some specificity except that all variable passed are of type string.
+# So here are some example of recommended usage:
+#
+# If you need a conditional block
+#
+# {% if should_my_block_be_shown == 'true' %}
+# ...
+# {% endif %}
+#
+# or
+#
+# {% if should_my_block_be_shown == '1' %}
+# ...
+# {% endif %}
+#
+# If you need to iterate with loop:
+#
+# {% for yolo in var_with_multiline_value.splitlines() %}
+# ...
+# {% endfor %}
+#
+# or
+#
+# {% for jail in my_var_with_coma.split(',') %}
+# ...
+# {% endfor %}
+#
+ynh_add_jinja_config() {
+    # Declare an array to define the options of this helper.
+    local legacy_args=tdi
+    local -A args_array=([t]=template= [d]=destination= [i]=ignore_vars= )
+    local template
+    local destination
+    local ignore_vars
+    # Manage arguments with getopts
+    ynh_handle_getopts_args "$@"
+    local template_path
 
-	if echo $main_device | grep --quiet "mmc" && [ $(tail -n1 /sys/block/$main_device/queue/rotational) == "0" ]
-	then
-		return 0
-	else
-		return 1
-	fi
+    #
+    ## List of all vars ignored and not passed to the template
+    # WARNING Update the list on the helper documentation at the top of the helper, if you change this list
+    #
+
+    # local vars used in the helper
+    ignore_vars+=" legacy_args args_array template destination ignore_vars template_path python_env_var ignore_var_regex"
+    # yunohost helpers
+    ignore_vars+=" progress_scale progress_string0 progress_string1 progress_string2"
+    # Arrays used in config panel
+    ignore_vars+=" old changed binds types file_hash formats"
+
+    if [ -f "$YNH_APP_BASEDIR/conf/$template" ]; then
+        template_path="$YNH_APP_BASEDIR/conf/$template"
+    elif [ -f "$template" ]; then
+        template_path=$template
+    else
+        ynh_die --message="The provided template $template doesn't exist"
+    fi
+
+    ynh_backup_if_checksum_is_different --file="$destination"
+
+    # Make sure to set the permissions before we copy the file
+    # This is to cover a case where an attacker could have
+    # created a file beforehand to have control over it
+    # (cp won't overwrite ownership / modes by default...)
+    touch "$destination"
+    chown root:root "$destination"
+    chmod 640 "$destination"
+
+    local python_env_var=''
+    local ignore_var_regex
+    ignore_var_regex="$(echo "$ignore_vars" | sed -E 's@^\s*(.*\w)\s*$@\1@g' | sed -E 's@(\s+)@|@g')"
+    while read -r one_var; do
+        # Blacklist of var to not pass to template
+        if { [[ "$one_var" =~ ^[A-Z0-9_]+$ ]] && [[ "$one_var" != YNH_* ]]; } \
+            || [[ "$one_var" =~ ^($ignore_var_regex)$ ]]; then
+            continue
+        fi
+        # Well python is very bad for the last character on raw string
+        # https://stackoverflow.com/questions/647769/why-cant-pythons-raw-string-literals-end-with-a-single-backslash
+        # So the solution here is to add one last char '-' so we know what it is
+        # and we are sure that it not \ or ' or something else which will be problematic with python
+        # And then we remove it while we are processing
+        python_env_var+="$one_var=r'''${!one_var}-'''[:-1],"
+    done <<< "$(compgen -v)"
+
+    _ynh_apply_default_permissions "$destination"
+    (
+        python3 -c 'import os, sys, jinja2; sys.stdout.write(
+                    jinja2.Template(source=sys.stdin.read(),
+                                    undefined=jinja2.StrictUndefined,
+                    ).render('"$python_env_var"'));' <"$template_path" >"$destination"
+    )
+    ynh_store_file_checksum --file="$destination"
 }
