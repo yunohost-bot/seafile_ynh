c108375dba65e05203f7c9760b01a0711717da3a
Josué Tille <josue@tille.ch>
2024-06-01 11:19:38 +0200

    Fix upgrade from old version

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -37,7 +37,7 @@ run_seafile_cmd() {
 }
 
 install_source() {
-    ynh_setup_source --dest_dir="$install_dir"/seafile_image --full_replace
+    ynh_setup_source_custom --dest_dir="$install_dir"/seafile_image --full_replace
     mkdir -p "$install_dir"/seafile_image/opt/seafile/{seafile-data,seahub-data,conf,logs}
     grep "^$app:x"  /etc/passwd | sed "s|$install_dir|/opt/seafile|" >> "$install_dir"/seafile_image/etc/passwd
     grep "^$app:x"  /etc/group >> "$install_dir"/seafile_image/etc/group
